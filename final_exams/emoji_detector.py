0e729f3d7d447add77aa47eaf2d08ad3a79a8bec
yordan-marinov <jordanmarinov8@gmail.com>
2020-12-06 13:12:58 +0200

    Updated file: 'emoji_detector'

--- final_exams/emoji_detector.py
+++ final_exams/emoji_detector.py
@@ -2,46 +2,31 @@ from functools import reduce
 import re
 
 
-def cool_threshold(string: str) -> int:
-    return reduce(
-        lambda x, y: x * y,
-        [
-            int(number)
-            for number in string
-            if number.isdigit()
-        ]
-    )
+def cool_threshold_number(string: str) -> int:
+    matched = [int(digit) for digit in re.findall(r"\d", string)]
+
+    return reduce(lambda x, y: x * y, matched)
 
 
-def get_matched(pattern: str, string: str) -> dict:
-    return {
-        match.group(): sum(
-            [ord(character) for character in match.group(2)
-             if character not in SURROUNDED_SYMBOLS]
-        )
-        for match in re.finditer(pattern, string)
-    }
+def emoji_ascii_value(string: str) -> int:
+    return sum([ord(letter) for letter in string])
 
 
-def print_statement(string: str, matched_dict: dict) -> print:
-    print(f"Cool threshold: {cool_threshold(string)}")
+def cool_emojis_detector_print(line: str) -> print:
+    cool_threshold = cool_threshold_number(line)
+
+    regex = r"(::|\*\*)([A-Z][a-z]{2,})\1"
+
+    print(f"Cool threshold: {cool_threshold}")
 
     print(
-        f"{len(matched_dict)} emojis found in the text. "
+        f"{len(re.findall(regex, line))} emojis found in the text. "
         f"The cool ones are:"
     )
-    [
-        print(emoji)
-        for emoji, value in matched_dict.items()
-        if value >= cool_threshold(string)
-    ]
+
+    for emoji in re.finditer(regex, line):
+        if emoji_ascii_value(emoji.group(2)) > cool_threshold:
+            print(emoji.group())
 
 
-SURROUNDED_SYMBOLS = (":", "*")
-
-strings = input()
-
-regex = r"(\*\*|::)([A-Z][a-z][a-z]+)\1"
-matched = get_matched(regex, strings)
-
-print_statement(strings, matched)
+cool_emojis_detector_print(input())
