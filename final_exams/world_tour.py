0579c0132226555ac103049a1d8ff0b33d4af38f
yordan-marinov <jordanmarinov8@gmail.com>
2020-12-08 15:48:47 +0200

    Updated file: 'world_tour'

--- final_exams/world_tour.py
+++ final_exams/world_tour.py
@@ -1,37 +1,47 @@
-def add_stop(line: str, *args) -> str:
+def add_stop(string: str, *args) -> str:
     index = int(args[0])
-    destination = args[1]
+    substring = args[1]
 
-    if index in range(len(line)):
-        line = line[:index] + destination + line[index:]
+    if index in range(len(string)):
+        string = string[:index] + substring + string[index:]
 
-    return line
+    return string
 
 
-def remove_stop(line: str, *args) -> str:
+def remove_stop(string: str, *args) -> str:
     start_index = int(args[0])
     end_index = int(args[1])
 
-    if (
-            start_index in range(len(line)) and
-            end_index in range(len(line))
-    ):
-        line = line[:start_index] + line[end_index + 1:]
+    if start_index in range(len(string)) and end_index in range(len(string)):
+        string = string.replace(string[start_index:end_index + 1], "")
 
-    return line
+    return string
 
 
-def switch(line: str, *args) -> str:
-    old_part = args[0]
-    new_part = args[1]
+def switch(string: str, *args) -> str:
+    substring = args[0]
+    new_substring = args[1]
 
-    if old_part in line:
-        line = line.replace(old_part, new_part)
+    string = string.replace(substring, new_substring)
 
-    return line
+    return string
 
 
-string = input()
+def main_manipulation_print_func(string: str) -> print:
+    while True:
+
+        data = input()
+        if data == "Travel":
+            print(f"Ready for world tour! Planned stops: {string}")
+            break
+
+        data = data.split(":")
+        command = data.pop(0)
+
+        string = COMMANDS[command](string, *data)
+
+        print(string)
+
 
 COMMANDS = {
     "Add Stop": add_stop,
@@ -39,19 +49,5 @@ COMMANDS = {
     "Switch": switch,
 }
 
-while True:
-
-    data = input()
-    if data == "Travel":
-        print(
-            f"Ready for world tour! "
-            f"Planned stops: {string}"
-        )
-        break
-
-    data = data.split(":")
-    command = data.pop(0)
-
-    string = COMMANDS[command](string, *data)
-
-    print(string)
+stops: str = input()
+main_manipulation_print_func(stops)
