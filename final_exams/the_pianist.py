68343f57212513d1c1d872db8804e8c12f0f0402
yordan-marinov <jordanmarinov8@gmail.com>
2020-12-09 19:37:40 +0200

    Updated file: 'the_pianist'

--- final_exams/the_pianist.py
+++ final_exams/the_pianist.py
@@ -1,94 +1,84 @@
-def get_data(n: int) -> dict:
-    result = {}
+def get_data() -> dict:
+    n = int(input())
+    pieces_data = {}
     for _ in range(n):
         data = input().split("|")
         piece = data[0]
         composer = data[1]
         key = data[2]
 
-        result[piece] = {"composer": composer, "key": key}
-
-    return result
-
-
-def add_elements(dd: dict, *args) -> dict:
-    piece, composer, key = args
-
-    if piece in dd:
-        print(
-            f"{piece} is already in the collection!"
-        )
-        return dd
-
-    dd[piece] = {"composer": composer, "key": key}
-    print(
-        f"{piece} by {composer} in {key} "
-        f"added to the collection!"
-    )
-
-    return dd
+        pieces_data[piece] = {
+            "composer": composer,
+            "key": key
+        }
+    return pieces_data
 
 
-def remove_elements(dd: dict, *args) -> dict:
-    piece = args[0]
+def add_data(dd: dict, *args):
+    current_piece, current_composer, current_key = args
 
-    if piece in dd:
-        print(f"Successfully removed {piece}!")
-        del dd[piece]
-
+    if current_piece in dd:
+        print(f"{current_piece} is already in the collection!")
     else:
         print(
-            f"Invalid operation! {piece} "
-            f"does not exist in the collection."
+            f"{current_piece} by {current_composer} in "
+            f"{current_key} added to the collection!"
         )
+        dd[current_piece] = {
+            "composer": current_composer,
+            "key": current_key
+        }
 
-    return dd
 
+def remove_data(dd: dict, *args):
+    current_piece = args[0]
 
-def change_key(dd: dict, *args) -> dict:
-    piece, new_key = args
-
-    if piece in dd:
-        print(f"Changed the key of {piece} to {new_key}!")
-        dd[piece]["key"] = new_key
-
+    if current_piece in dd:
+        print(f"Successfully removed {current_piece}!")
+        del dd[current_piece]
     else:
         print(
             f"Invalid operation! "
-            f"{piece} does not exist in the collection."
+            f"{current_piece} does not exist in the collection."
         )
 
-    return dd
+
+def change_key(dd: dict, *args):
+    current_piece, new_key = args
+
+    if current_piece in dd:
+        print(f"Changed the key of {current_piece} to {new_key}!")
+        dd[current_piece]["key"] = new_key
+    else:
+        print(
+            f"Invalid operation! "
+            f"{current_piece} does not exist in the collection."
+        )
 
 
-def main(dd: dict):
+def sorting_printing_func(dd: dict) -> print:
+    for piece, values in sorted(
+            dd.items(),
+            key=lambda pair: (pair[0], pair[1]["composer"])
+    ):
+        print(f"{piece} -> Composer: {values['composer']}, Key: {values['key']}")
+
+
+def main_manipulation_print_func(dd: dict, commands) -> print:
     while True:
 
         data = input()
         if data == "Stop":
-            print_statement(dd)
+            sorting_printing_func(dd)
             break
 
         data = data.split("|")
         command = data.pop(0)
 
-        COMMANDS[command](dd, *data)
+        commands[command](dd, *data)
 
 
-def print_statement(dd: dict) -> print:
-    for name_piece, values in sorted(
-            dd.items(),
-            key=lambda x: (x[0], x[1]["composer"])
-    ):
-        print(f"{name_piece} -> Composer: {values['composer']}, Key: {values['key']}")
+COMMANDS = dict(Add=add_data, Remove=remove_data, ChangeKey=change_key)
 
-
-COMMANDS = {
-    "Add": add_elements,
-    "Remove": remove_elements,
-    "ChangeKey": change_key,
-}
-
-count = int(input())
-pieces: dict = get_data(count)
-main(pieces)
+pieces = get_data()
+main_manipulation_print_func(pieces, COMMANDS)
