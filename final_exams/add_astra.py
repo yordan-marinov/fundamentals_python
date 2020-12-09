6d37efe6f593fb67c85640b45ee1ec793bef29e0
yordan-marinov <jordanmarinov8@gmail.com>
2020-12-09 16:03:48 +0200

    Updated file: 'add_astra'

--- final_exams/add_astra.py
+++ final_exams/add_astra.py
@@ -1,22 +1,23 @@
 import re
 
-DAY_CALORIES = 2000
-regex = r"(#|\|)([a-zA-Z ]+)\1(\d{2}/\d{2}/\d{2})\1(\d+)\1"
+CALORIES_A_DAY = 2_000
 
-text = input()
+regex = r"(#|\|)([a-zA-Z\s]+)\1(\d{2}/\d{2}/\d{2})\1([0-9][0-9]{,3}|10000)\1"
 
-total_calories = sum(
-    [
-        int(calorie.group(4))
-        for calorie in re.finditer(regex, text)
-    ]
+text_string = input()
+
+matched = re.findall(regex, text_string)
+
+total_calories = sum(int(product[3]) for product in matched)
+
+print(
+    f"You have food to last you for: "
+    f"{total_calories // CALORIES_A_DAY} days!"
 )
-days = total_calories // DAY_CALORIES
 
-print(f"You have food to last you for: {days} days!")
-for match in re.finditer(regex, text):
+for product in matched:
     print(
-        f"Item: {match.group(2)}, "
-        f"Best before: {match.group(3)}, "
-        f"Nutrition: {match.group(4)}"
+        f"Item: {product[1]}, "
+        f"Best before: {product[2]}, "
+        f"Nutrition: {product[3]}"
     )
