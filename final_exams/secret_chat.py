c1fc7f4b970642559818bf4e31c11eec1f005357
yordan-marinov <jordanmarinov8@gmail.com>
2020-12-11 14:16:33 +0200

    Updated file: 'secret_chat'

--- final_exams/secret_chat.py
+++ final_exams/secret_chat.py
@@ -3,50 +3,49 @@ def insert_space(string: str, *args) -> str:
 
     string = string[:index] + " " + string[index:]
     print(string)
+
     return string
 
 
-def reverse(string: str, *args) -> str:
+def reverse_string(string: str, *args) -> str:
     substring = args[0]
 
     if substring not in string:
         print("error")
-    else:
-        string = string.replace(substring, "", 1)
-        string = string + substring[::-1]
-        print(string)
+        return string
+
+    string = string.replace(substring, "", 1)
+    string = string + substring[::-1]
+    print(string)
+
     return string
 
 
 def change_all(string: str, *args) -> str:
-    substring = args[0]
-    new_substring = args[1]
+    substring, new_substring = args
 
     string = string.replace(substring, new_substring)
     print(string)
+
     return string
 
 
-def main():
-    massage = input()
+def main_manipulation_print_func(commands: dict) -> print:
+    concealed_message = input()
 
     while True:
-
         data = input()
         if data == "Reveal":
-            print(f"You have a new text message: {massage}")
+            print(f"You have a new text message: {concealed_message}")
             break
 
         data = data.split(":|:")
         command = data.pop(0)
 
-        massage = COMMANDS[command](massage, *data)
+        concealed_message = commands[command](concealed_message, *data)
 
 
-COMMANDS = {
-    "InsertSpace": insert_space,
-    "Reverse": reverse,
-    "ChangeAll": change_all,
-}
+COMMANDS = dict(InsertSpace=insert_space, Reverse=reverse_string, ChangeAll=change_all)
 
-main()
+
+main_manipulation_print_func(COMMANDS)
