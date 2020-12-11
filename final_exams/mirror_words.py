6cd510679c164a54cdbada5265440328cf73f2a9
yordan-marinov <jordanmarinov8@gmail.com>
2020-12-11 14:54:03 +0200

    Updated file: 'mirror_words'

--- final_exams/mirror_words.py
+++ final_exams/mirror_words.py
@@ -1,23 +1,22 @@
 import re
 
-regex = r"(#|@)([a-zA-Z]{3,})\1\1([a-zA-Z]{3,})"
+regex = r"(@|#)([a-zA-Z]{3,})\1\1([a-zA-Z]{3,})\1"
 
-text_string = input()
+string = input()
 
-matched = re.findall(regex, text_string)
-
+matched = re.findall(regex, string)
 if not matched:
     print("No word pairs found!")
 else:
     print(f"{len(matched)} word pairs found!")
 
-mirror_words = []
-for match in matched:
-    if match[1] == match[2][::-1]:
-        mirror_words.append(" <=> ".join(match[1:]))
-
-if mirror_words:
+mirror_words = [
+    ' <=> '.join([word.group(2), word.group(3)])
+    for word in re.finditer(regex, string)
+    if word.group(2) == word.group(3)[::-1]
+]
+if not mirror_words:
+    print("No mirror words!")
+else:
     print("The mirror words are:")
     print(*mirror_words, sep=", ")
-else:
-    print("No mirror words!")
