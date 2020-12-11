2e061709f6615d433a36ae6beb682959ad09f28a
yordan-marinov <jordanmarinov8@gmail.com>
2020-12-11 11:30:50 +0200

    Updated file: 'fancy_barcodes'

--- final_exams/fancy_barcodes.py
+++ final_exams/fancy_barcodes.py
@@ -3,20 +3,15 @@ import re
 regex = r"(@#+)([A-Z][a-zA-Z0-9]{4,}[A-Z])(@#+)"
 
 count_of_barcodes = int(input())
-
 for _ in range(count_of_barcodes):
-    data = input()
-    matched = re.findall(regex, data)
+    matched = re.match(regex, input())
     if not matched:
         print("Invalid barcode")
-        continue
+    else:
+        match = re.findall(r"\d", matched.group(2))
+        if not match:
+            match = '00'
+        else:
+            match = "".join(match)
 
-    result = ""
-    for c in matched[0][1]:
-        if c.isdigit():
-            result += c
-
-    if not result:
-        result = "00"
-
-    print(f"Product group: {result}")
+        print(f"Product group: {match}")
