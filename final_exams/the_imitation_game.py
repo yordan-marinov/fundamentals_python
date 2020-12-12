68039ce76bce329498d31e6211d3c1a196b5d216
yordan-marinov <jordanmarinov8@gmail.com>
2020-12-12 16:14:26 +0200

    Updated file: 'the_imitation_game'

--- final_exams/the_imitation_game.py
+++ final_exams/the_imitation_game.py
@@ -1,7 +1,23 @@
+def main_manipulation_print_func(string: str, commands) -> print:
+    while True:
+        data = input()
+        if data == "Decode":
+            print(f"The decrypted message is: {string}")
+            break
+
+        data = data.split("|")
+        command = data.pop(0)
+
+        string = commands[command](string, *data)
+
+
 def move(string: str, *args) -> str:
     number_of_letters = int(args[0])
 
-    string = string[number_of_letters:] + string[: number_of_letters]
+    substring = string[:number_of_letters]
+    string = string.replace(substring, "")
+    string += substring
+
     return string
 
 
@@ -10,6 +26,7 @@ def insert(string: str, *args) -> str:
     value = args[1]
 
     string = string[:index] + value + string[index:]
+
     return string
 
 
@@ -22,21 +39,7 @@ def change_all(string: str, *args) -> str:
     return string
 
 
-def main_manipulation_printing_func(string: str, commands: dict) -> print:
-    while True:
-
-        data = input()
-        if data == "Decode":
-            print(f"The decrypted message is: {string}")
-            break
-
-        data = data.split("|")
-        command = data.pop(0)
-
-        string = commands[command](string, *data)
-
-
 COMMANDS = dict(Move=move, Insert=insert, ChangeAll=change_all)
 
 encrypted_message = input()
-main_manipulation_printing_func(encrypted_message, COMMANDS)
+main_manipulation_print_func(encrypted_message, COMMANDS)
